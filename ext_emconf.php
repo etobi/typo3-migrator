5db5ab4c9207efb11e688fc3a5cd11853087a9f7
Tobias Liebig <work@etobi.de>
2020-09-02 09:21:14 +0200

    TASK code cleanup

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,17 +3,17 @@
  * Extension Manager/Repository config file for ext "migrator".
  ***************************************************************/
 $EM_CONF['migrator'] = array(
-	'title' => 'DB Migrator',
-	'description' => 'TYPO3 DB Migrator',
-	'category' => 'be',
-	'state' => 'beta',
-	'author' => 'Sebastian Michaelsen',
-	'author_email' => 'sebastian@app-zap.de',
-	'author_company' => 'app zap',
-	'version' => '1.2.0',
-	'constraints' => array(
-		'depends' => array(
-			'typo3' => '8.7.0-9.5.99',
-		),
-	),
+    'title' => 'Migrator',
+    'description' => 'TYPO3 Migrator',
+    'category' => 'be',
+    'state' => 'beta',
+    'author' => 'Sebastian Michaelsen',
+    'author_email' => 'sebastian@app-zap.de',
+    'author_company' => 'app zap',
+    'version' => '2.0.0',
+    'constraints' => array(
+        'depends' => array(
+            'typo3' => '10.0.0-10.99.99',
+        ),
+    ),
 );
