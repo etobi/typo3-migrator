6d690433d36004c9c95bc47fe537b7f398af7606
Tobias Liebig <mail@etobi.de>
2021-03-18 14:36:44 +0100

    TASK fix ext_localconf

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,3 +1,3 @@
 <?php
 
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers'][] = \AppZap\Migrator\Command\MigrationCommandController::class
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers'][] = \AppZap\Migrator\Command\MigrationCommandController::class;
